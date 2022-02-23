0c5fb2ae2bd0734df17ddb568cebc34048fbcdd5
Guillaume Lambert <guil.lambert@protonmail.com>
2022-02-23 15:21:30 -0500

    added gas price to footer

--- src/components/Header/Polling.tsx
+++ src/components/Header/Polling.tsx
@@ -1,3 +1,7 @@
+import { Trans } from '@lingui/macro'
+import { RowFixed } from 'components/Row'
+import useGasPrice from 'hooks/useGasPrice'
+import JSBI from 'jsbi'
 import { useEffect, useState } from 'react'
 import { useAppSelector } from 'state/hooks'
 import styled, { keyframes } from 'styled-components/macro'
@@ -6,6 +10,7 @@ import { useActiveWeb3React } from '../../hooks/web3'
 import { useBlockNumber } from '../../state/application/hooks'
 import { ExternalLink, TYPE } from '../../theme'
 import { ExplorerDataType, getExplorerLink } from '../../utils/getExplorerLink'
+import { MouseoverTooltip } from '../Tooltip'
 import { ChainConnectivityWarning } from './ChainConnectivityWarning'
 
 const StyledPolling = styled.div<{ warning: boolean }>`
@@ -38,6 +43,17 @@ const StyledPollingDot = styled.div<{ warning: boolean }>`
   position: relative;
   background-color: ${({ theme, warning }) => (warning ? theme.yellow3 : theme.green1)};
   transition: 250ms ease background-color;
+`
+
+const StyledGasDot = styled.div`
+  background-color: ${({ theme }) => theme.green1};
+  border-radius: 50%;
+  height: 4px;
+  min-height: 4px;
+  min-width: 4px;
+  position: relative;
+  transition: 250ms ease background-color;
+  width: 4px;
 `
 
 const rotate360 = keyframes`
@@ -74,6 +90,8 @@ export default function Polling() {
   const [isMounting, setIsMounting] = useState(false)
   const [isHover, setIsHover] = useState(false)
   const chainConnectivityWarning = useAppSelector((state) => state.application.chainConnectivityWarning)
+  const ethGasPrice = useGasPrice()
+  const priceGwei = ethGasPrice ? JSBI.divide(ethGasPrice, JSBI.BigInt(1000000000)) : undefined
 
   useEffect(
     () => {
@@ -95,23 +113,44 @@ export default function Polling() {
 
   return (
     <>
-      <ExternalLink
-        href={chainId && blockNumber ? getExplorerLink(chainId, blockNumber.toString(), ExplorerDataType.BLOCK) : ''}
-      >
-        <StyledPolling
-          onMouseEnter={() => setIsHover(true)}
-          onMouseLeave={() => setIsHover(false)}
-          warning={chainConnectivityWarning}
-        >
+      <RowFixed>
+        <StyledPolling onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} warning={false}>
+          <ExternalLink href={'https://etherscan.io/gastracker'}>
+            {priceGwei ? (
+              <RowFixed style={{ marginRight: '8px' }}>
+                <TYPE.small mr="8px">
+                  <MouseoverTooltip
+                    text={
+                      <Trans>
+                        {`The current fast gas amount for sending a transaction on L1.
+                    Gas fees are paid in Ethereum's native currency Ether (ETH) and denominated in gwei. `}
+                      </Trans>
+                    }
+                  >
+                    {priceGwei.toString()} <Trans>gwei</Trans>
+                  </MouseoverTooltip>
+                </TYPE.small>
+                <StyledGasDot />
+              </RowFixed>
+            ) : null}
+          </ExternalLink>
           <StyledPollingNumber breathe={isMounting} hovering={isHover}>
-            {blockNumber}&ensp;
+            <ExternalLink
+              href={
+                chainId && blockNumber ? getExplorerLink(chainId, blockNumber.toString(), ExplorerDataType.BLOCK) : ''
+              }
+            >
+              <MouseoverTooltip
+                text={<Trans>{`The most recent block number on this network. Prices update on every block.`}</Trans>}
+              >
+                {blockNumber}&ensp;
+              </MouseoverTooltip>
+            </ExternalLink>
           </StyledPollingNumber>
-          <StyledPollingDot warning={chainConnectivityWarning}>
-            {isMounting && <Spinner warning={chainConnectivityWarning} />}
-          </StyledPollingDot>{' '}
+          <StyledPollingDot warning={false}>{isMounting && <Spinner warning={false} />}</StyledPollingDot>{' '}
         </StyledPolling>
-      </ExternalLink>
-      {chainConnectivityWarning && <ChainConnectivityWarning />}
+        {false && <ChainConnectivityWarning />}
+      </RowFixed>
     </>
   )
 }
